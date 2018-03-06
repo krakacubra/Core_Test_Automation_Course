103250f8c3fa59f804e444a9541d41aa25067811
krakacubra <anja.kazakovva@gmail.com>
2018-03-07 01:00:25 +0300

    enum PaperE was moved to separate file nPaper field and getter and setter for it were added methods equals, hashCode, toString were added

--- 02_JavaFundamentals_practice/src/main/java/pen/stationery/Paper.java
+++ 02_JavaFundamentals_practice/src/main/java/pen/stationery/Paper.java
@@ -1,15 +1,60 @@
 package main.java.pen.stationery;
 
-enum PaperE {
-    NOTEBOOK,
-    PAPER;
-}
-
-
 public class Paper
 extends Stationery{
     PaperE item;
+    int nPages;
+    public Paper(){
+        super();
+    }
+    public Paper(PaperE item, int nPages){
+        super();
+        this.item = item;
+        this.nPages = nPages;
+    }
+    public Paper(PaperE item, int nPages, float price){
+        super(price);
+        this.item = item;
+        this.nPages = nPages;
+    }
+    public Paper(PaperE item, int nPages, float price, String brand){
+        super(brand, price);
+        this.item = item;
+        this.nPages = nPages;
+    }
 
+    @Override
+    public String toString() {
+        return super.toString() + ", item: " + item + ", number of pages: " + nPages;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode() + item.hashCode() + 23 * nPages;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (super.equals(obj)){
+            Paper paper = (Paper)obj;
+            if (paper.nPages != this.nPages){
+                return false;
+            }
+            if (this.item == null){
+                return (this.item == paper.item);
+            }
+            return this.item.equals(paper.item);
+        }
+        return false;
+    }
+
+    public int getnPages() {
+        return nPages;
+    }
+
+    public void setnPages(int nPages) {
+        this.nPages = nPages;
+    }
     public void setItem(PaperE item) {
         this.item = item;
     }
@@ -17,4 +62,5 @@ extends Stationery{
     public PaperE getItem() {
         return item;
     }
+
 }
---
+++ 02_JavaFundamentals_practice/src/main/java/pen/stationery/PaperE.java
@@ -0,0 +1,8 @@
+package main.java.pen.stationery;
+
+public enum PaperE {
+    NOTEBOOK,
+    PAPER,
+    ALBBUM,
+    STICKERS;
+}
