74712e596b780fb796b6bbf7f5055105650b067f
krakacubra <anja.kazakovva@gmail.com>
2018-03-17 21:26:53 +0300

    StationeryNameComparator was redone

--- 02_JavaFundamentals_practice/src/main/java/pen/stationery/comparators/StationeryNameComparator.java
+++ 02_JavaFundamentals_practice/src/main/java/pen/stationery/comparators/StationeryNameComparator.java
@@ -6,6 +6,8 @@ import main.java.pen.stationery.Stationery;
 import main.java.pen.stationery.WritingInstruments;
 
 import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,10 +15,14 @@ public class StationeryNameComparator
 implements Comparator<Stationery>{
     @Override
     public int compare(Stationery o1, Stationery o2) {
-
-        if (o1.toString().compareTo(o2.toString()) < 0) {
+        Pattern patternToItem = Pattern.compile(".+@ item: (\\w+).+");
+        Matcher match1 = patternToItem.matcher(o1.toString());
+        Matcher match2 = patternToItem.matcher(o2.toString());
+        match1.find();
+        match2.find();
+        if (match1.group(1).compareTo(match2.group(1)) < 0) {
             return -1;
         }
-        return o1.toString().compareTo(o2.toString()) > 0 ? 1 : 0;
+        return match1.group(1).compareTo(match2.group(1)) > 0 ? 1 : 0;
     }
 }
